008ca41b83a64f762b085cdaaa1ecf1391adc2a0
Swadhin Samal <swadhinsamal1999@gmail.com>
2026-04-24 12:52:47 +0530

    Just Updated with Minor changes 1

--- playwright.config.js
+++ playwright.config.js
@@ -1,15 +1,5 @@
-// @ts-check
 import { defineConfig } from '@playwright/test';
 import dotenv from 'dotenv';
-
-// Load environment variables
-/*dotenv.config({
-  path: `.env.${process.env.ENV || 'dev'}`
-});*/
-
-/*dotenv.config({
-  path: `.env.${process.env.ENV || 'qa'}`
-});*/
 
 const ENV = process.env.ENV || 'qa';
 
@@ -20,59 +10,40 @@ dotenv.config({
 export default defineConfig({
   testDir: './tests',
 
-  /* Global timeouts */
+  globalSetup: './global-setup.js',
+
   timeout: 30000,
   expect: {
     timeout: 5000,
   },
 
-  /* Execution settings */
   fullyParallel: true,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
 
-  /* Reporters */
   reporter: [
     ['html'],
     ['list']
   ],
 
-  /* Shared settings */
   use: {
     baseURL: process.env.BASE_URL || "https://demoqa.com",
-  navigationTimeout: 30000,
+    navigationTimeout: 45000,
     actionTimeout: 10000,
-
-    //baseURL: process.env.BASE_URL,
-    //storageState: 'auth.json',
     headless: true,
-
     viewport: { width: 1280, height: 720 },
-
     ignoreHTTPSErrors: true,
-
     screenshot: 'only-on-failure',
     video: 'retain-on-failure',
     trace: 'on-first-retry',
-    globalSetup: './global-setup.js',
   },
 
-  /* Browser projects */
   projects: [
     {
       name: 'chromium',
       use: { browserName: 'chromium' },
-    },
-    {
-      name: 'firefox',
-      use: { browserName: 'firefox' },
-    },
-    {
-      name: 'webkit',
-      use: { browserName: 'webkit' },
-    },
+    }
   ],
 
-  /* Optional: Folder for test artifacts */
   outputDir: 'test-results/',
 });
